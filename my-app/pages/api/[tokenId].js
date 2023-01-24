13337015bd7c679236f2087fecb28f6d11885ee1
CS268 <Matovuruky@gmail.com>
2023-01-24 22:25:47 +0100

    changed all the image url in metadata foleder

--- my-app/pages/api/[tokenId].js
+++ my-app/pages/api/[tokenId].js
@@ -6,21 +6,7 @@ export default function handler(req, res) {
     // Generate the image_url based on the tokenId
     const image_url = `https://ipfs.io/ipfs/QmYPS6pTMPBz7ibZgxBvyi5bqDGs5oGNRFijnACB2Lux53/${tokenId}.png`;
     // Send back the metadata for the NFT
-    res.status(200).json({
-        name: `colorfulAI #${tokenId}`,
-        description: "colorfulAI is a collection of AI Art for supporters in crypto",
-        image: image_url
-    });
-}
-export default function handler(req, res) {
-    // Get the tokenId from the query params
-    const tokenId = req.query.tokenId;
-    // Generate the tokenURI based on the tokenId
-    const tokenURI = `https://colorfuls-ai.vercel.app/api/tokenId?tokenId=${tokenId}`;
-    // Generate the image_url based on the tokenId
-    const image_url = `https://ipfs.io/ipfs/QmYPS6pTMPBz7ibZgxBvyi5bqDGs5oGNRFijnACB2Lux53/${tokenId}.png`;
-    // Send back the metadata for the NFT
-    res.status(200).json({
+    return res.status(200).json({
         name: `colorfulAI #${tokenId}`,
         description: "colorfulAI is a collection of AI Art for supporters in crypto",
         image: image_url
